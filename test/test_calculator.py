58891fed63ef2507a6b180163259c367f9ada41b
María García <mariav2601@gmail.com>
2024-11-07 14:33:25 -0400

    update

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,4 +1,7 @@
 import unittest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 from src.calculator import sum, subtract, multiply, divide
 
