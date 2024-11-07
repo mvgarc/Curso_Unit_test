c1959c32e4671b6bdf94508957127423d3b2d2fd
María García <mariav2601@gmail.com>
2024-11-07 19:34:27 -0400

    updated

--- src/api_client.py
+++ src/api_client.py
@@ -4,3 +4,5 @@ def get_location(ip):
     url = f"https://freeipapi.com/api/json/{ip}"
     response = requests.get(url)
     response.raise_for_status()
+    data = response.json()
+    return data
