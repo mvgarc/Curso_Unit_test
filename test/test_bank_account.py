76aa9066d386c46d210df987e232b977208b104a
María García <mariav2601@gmail.com>
2024-11-07 14:52:00 -0400

    se crea el metodo de deposito

--- test/test_bank_account.py
+++ test/test_bank_account.py
@@ -7,3 +7,5 @@ class BankAccountTests (unittest.TestCase):
         self.account = BankAccount(balance=1000)
 
     def test_deposit(self):
+        new_balance = self.account.deposit(500)
+        assert new_balance == 1500
