abdf15fbbaedf6ef033349eaa8b82c49367e1d64
María García <mariav2601@gmail.com>
2024-11-07 14:36:06 -0400

    crecaion de la funcion desposito donde se le va abonar dinero a la cuenta

--- src/bank_account.py
+++ src/bank_account.py
@@ -1,3 +1,8 @@
 class BankAccount:
     def __init__(self, balance=0):
         self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+        return self.balance
