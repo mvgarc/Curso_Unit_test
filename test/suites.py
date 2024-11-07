06651e57aa625695fb935c82b3a84d88c152d808
María García <mariav2601@gmail.com>
2024-11-07 18:42:00 -0400

    updated

--- test/suites.py
+++ test/suites.py
@@ -1,1 +1,14 @@
 import unittest
+
+from test.test_bank_account import BankAccountTests
+
+
+def bank_account_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(BankAccountTests("test_deposit"))
+    suite.addTest(BankAccountTests("test_withdraw"))
+    return suite
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    runner.run(bank_account_suite())
