6df8e3f9b72052da4fea752542a2d7c27aa46afa
María García <mariav2601@gmail.com>
2024-11-07 17:31:56 -0400

    se crea la funcion para probar el assert si es true o false

--- test/test_all_asserts.py
+++ test/test_all_asserts.py
@@ -5,3 +5,5 @@ class AllAssertsTest(unittest.TestCase):
     def test_assert_equal(self):
         self.assertEqual(10, 10)
         self.assertEqual("Hola", "Hola")
+
+    def test_assert_true_or_false(self):
