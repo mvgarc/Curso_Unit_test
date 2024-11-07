0f3cc332c2ccb4fd4910d0b42791439329b5f6d1
María García <mariav2601@gmail.com>
2024-11-07 14:51:10 -0400

    funcion para inicializar las cuentas de prueba con un balance de 1000

--- test/test_bank_account.py
+++ test/test_bank_account.py
@@ -2,3 +2,6 @@ import unittest
 from src.bank_account import BankAccount
 
 class BankAccountTests (unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.account = BankAccount(balance=1000)
