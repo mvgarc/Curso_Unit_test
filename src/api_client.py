8ef527c2c269577fb8dd76dd41c2e4ad01d21ef3
María García <mariav2601@gmail.com>
2024-11-07 19:33:40 -0400

    dislexiahahahahha

--- src/api_client.py
+++ src/api_client.py
@@ -2,4 +2,5 @@ import requests
 
 def get_location(ip):
     url = f"https://freeipapi.com/api/json/{ip}"
-    reponse = requests.get(url)
+    response = requests.get(url)
+    response.raise_for_status()
