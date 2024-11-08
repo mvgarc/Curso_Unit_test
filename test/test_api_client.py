86b9a4f8b8d760b3000a94684191b88f4c73c44d
María García <mariav2601@gmail.com>
2024-11-08 10:20:59 -0400

    se añade la direccion de google

--- test/test_api_client.py
+++ test/test_api_client.py
@@ -4,4 +4,4 @@ from src.api_client import get_location
 class ApiClientTest(unittest.TestCase):
 
     def test_get_location_returns_expected_data(self):
-        data = get_location("")
+        data = get_location("8.8.8.8")
