d3692058a27968bb3925fcf9668e9db9290e44aa
María García <mariav2601@gmail.com>
2024-11-08 10:20:27 -0400

    crecaion de la funcion test_get_location_returns_expected_data

--- test/test_api_client.py
+++ test/test_api_client.py
@@ -1,3 +1,7 @@
 import unittest
+from src.api_client import get_location
 
 class ApiClientTest(unittest.TestCase):
+
+    def test_get_location_returns_expected_data(self):
+        data = get_location("")
