94c40a2546eb89329b400ad74c4b5317cd536eab
María García <mariav2601@gmail.com>
2024-11-08 22:12:38 -0400

    updated

--- src/api_client.py
+++ src/api_client.py
@@ -5,6 +5,7 @@ def get_location(ip):
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
+    import ipdb; ipdb.set_trace()
     return {
         "country": data["countryName"],
         "region": data["regionName"],
