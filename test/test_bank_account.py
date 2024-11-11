da8f115b5f5db2f6192a6971b077580771a19d5c
María García <mariav2601@gmail.com>
2024-11-11 11:35:02 -0400

    crecaion de prueba para que no este permitido el retiro en horas no laborales

--- test/test_bank_account.py
+++ test/test_bank_account.py
@@ -53,3 +53,9 @@ class BankAccountTests (unittest.TestCase):
         mock_datetime.now.return_value.hour = 7
         with self.assertRaises(WithdrawalTimeRestrictionError):
             self.account.withdraw(100)
+
+    @patch("src.bank_account.datetime")
+    def test_withdraw_disallow_after_business_hours(self, mock_datetime):
+        mock_datetime.now.return_value.hour = 18
+        with self.assertRaises(WithdrawalTimeRestrictionError):
+            self.account.withdraw(100)
