0bff0d99d81b537be5516988f351ac7c8a3e8c5c
María García <mariav2601@gmail.com>
2024-11-12 14:42:54 -0400

    Creación de la clase UserTests

--- test/test_user.py
+++ test/test_user.py
@@ -3,3 +3,9 @@ from faker import Faker
 
 from src.user import User
 from src.bank_account import BankAccount
+
+class UserTests(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.faker = Faker(locale="es")
+        self.user = User(name=self.faker.name(), email=self.faker.email())
