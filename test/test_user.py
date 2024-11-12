8838f1bfc4427aa116baf68ca36dfa838eaefc73
María García <mariav2601@gmail.com>
2024-11-12 14:44:11 -0400

    Creación de la función test_user_creation, donde se verifica la creación del usuario usando assertEqual

--- test/test_user.py
+++ test/test_user.py
@@ -9,3 +9,10 @@ class UserTests(unittest.TestCase):
     def setUp(self) -> None:
         self.faker = Faker(locale="es")
         self.user = User(name=self.faker.name(), email=self.faker.email())
+
+    def test_user_creation(self):
+        name_generated = self.faker.name()
+        email_generated = self.faker.email()
+        user = User(name=name_generated, email=email_generated)
+        self.assertEqual(user.name, name_generated)
+        self.assertEqual(user.email, email_generated)
